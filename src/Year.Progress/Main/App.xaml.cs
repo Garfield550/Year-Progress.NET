f07d6afbf11bfd81a4d9dc09a8422cc6261ee2e4
Garfield Lee <garfield_550@outlook.com>
2018-01-06 16:56:58 +0800

    Clean code

--- src/Year.Progress/Main/App.xaml.cs
+++ src/Year.Progress/Main/App.xaml.cs
@@ -16,7 +16,6 @@
             // Ref: 
             // http://www.c-sharpcorner.com/UploadFile/f9f215/how-to-restrict-the-application-to-just-one-instance/
             // https://www.codeproject.com/Articles/1173686/A-Csharp-System-Tray-Application-using-WPF-Forms
-
             var mutexName = Assembly.GetExecutingAssembly().GetType().GUID.ToString();
             mutex = new Mutex(true, mutexName, out bool createdNew);
             if (!createdNew)
